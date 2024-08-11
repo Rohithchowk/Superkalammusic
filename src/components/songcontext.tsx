738430aa74cbd35edb5d4fd7b002d16113a3bf95
Rohithhowk <rohith28chowki@gmail.com>
2024-08-12 01:05:30 +0530

    finally

--- src/components/songcontext.tsx
+++ src/components/songcontext.tsx
@@ -5,7 +5,7 @@ import { createContext, useContext, useState } from 'react';
 const SongContext = createContext();
 
 export const SongProvider = ({ children }) => {
-  const [currentSong, setCurrentSong] = useState(null);
+  const [currentSong, setCurrentSong] = useState("kalki.mp3");
 
   return (
     <SongContext.Provider value={{ currentSong, setCurrentSong }}>
